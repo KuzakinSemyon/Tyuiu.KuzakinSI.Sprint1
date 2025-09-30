bf9e88cfff5e4521d0c9ace64e8e439c49be833c
KuzakinSemyon <kuzakinsemen@gmail.com>
2025-09-30 22:26:50 +0500

    Task5

---
+++ Tyuiu.KuzakinSI.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KuzakinSI.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.KuzakinSI.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = 0;
+            double y1 = 0;
+            double x2 = 4;
+            double y2 = 3;
+            Assert.AreEqual(5, ds.DistanceBetweenDots(x1, y1, x2, y2));
+        }
+    }
+}
--- Tyuiu.KuzakinSI.Sprint1.Task5.V1/Program.cs
+++ Tyuiu.KuzakinSI.Sprint1.Task5.V1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Runtime.ConstrainedExecution;
-using Tyuiu.KuzakinSI.Sprint1.Task4.V18.Lib;
+using Tyuiu.KuzakinSI.Sprint1.Task5.V1.Lib;
 
 Console.Title = "Спринт #1 | Выполнил: Кузякин Семён Игоревич | ПИНб-25-1";
 DataService ds = new DataService();
@@ -8,29 +8,36 @@ DataService ds = new DataService();
 Console.WriteLine("*******************************************************************");
 Console.WriteLine("* Спринт #1                                                       *");
 Console.WriteLine("* Тема: Базовые навыки работы в C#                                *");
-Console.WriteLine("* Задание #4                                                      *");
-Console.WriteLine("* Вариант #18                                                     *");
+Console.WriteLine("* Задание #5                                                      *");
+Console.WriteLine("* Вариант #1                                                     *");
 Console.WriteLine("* Выполнил: Кузякин Семён Игоревич | ПИНб-25-1                    *");
 Console.WriteLine("*******************************************************************");
 Console.WriteLine("* УСЛОВИЕ:                                                        *");
-Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, вычисляет результат по формуле*");
-Console.WriteLine("* и печатает его на экране. Ответ округлите до 3 знаков после запятой.*");
+Console.WriteLine("* Найти расстояние между двумя точками с заданными координатами (x, y). Ответ привести к целому с помощью класса Convert.*");
 Console.WriteLine("*                                                                 *");
 Console.WriteLine("*******************************************************************");
 Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                *");
 
-double x;
-Console.Write("x = ");
-x = Convert.ToDouble(Console.ReadLine());
+double x1;
+Console.Write("x1 = ");
+x1 = Convert.ToDouble(Console.ReadLine());
 
-double y;
-Console.Write("y = ");
-y = Convert.ToDouble(Console.ReadLine());
+double y1;
+Console.Write("y1 = ");
+y1 = Convert.ToDouble(Console.ReadLine());
+
+double x2;
+Console.Write("x2 = ");
+x2 = Convert.ToDouble(Console.ReadLine());
+
+double y2;
+Console.Write("y2 = ");
+y2 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("*******************************************************************");
 Console.WriteLine("* РЕЗУЛЬТАТ:                                                      *");
 Console.WriteLine("*******************************************************************");
 
-Console.WriteLine(ds.Calculate(x, y));
+Console.WriteLine(ds.DistanceBetweenDots(x1, y1, x2, y2));
 
 Console.ReadLine();
